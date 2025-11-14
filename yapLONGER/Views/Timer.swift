da95dd5b98aa9c2711bce26a736dd5cd6a548309
Ethan Soh <ethan.sohyt@gmail.com>
2025-11-14 13:44:58 +0800

    bruh.

---
+++ yapLONGER/Views/Timer.swift
@@ -0,0 +1,18 @@
+//
+//  Timer.swift
+//  yapLONGER
+//
+//  Created by Ethan Soh on 14/11/25.
+//
+
+import SwiftUI
+
+struct Timer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Timer()
+}
